9a11532451938eef59b532e4aa7c9367393658b4
AndrePoletto <polettoandre@gmail.com>
2017-11-17 17:51:45 -0200

    Adding about activity

---
+++ app/src/main/java/br/edu/utfpr/alunos/andrepoletto/gamescollection/ThirdActivity.java
@@ -0,0 +1,14 @@
+package br.edu.utfpr.alunos.andrepoletto.gamescollection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+    public static final int ASKRESULT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
