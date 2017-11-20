c1b47640616cbd8405caa91d016e4e33c3eaa5b1
AndrePoletto <polettoandre@gmail.com>
2017-11-20 18:05:57 -0200

    Update

--- app/src/main/java/br/edu/utfpr/alunos/andrepoletto/gamescollection/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/alunos/andrepoletto/gamescollection/MainActivity.java
@@ -3,6 +3,7 @@ package br.edu.utfpr.alunos.andrepoletto.gamescollection;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     public void showAboutActivity(View view) {
